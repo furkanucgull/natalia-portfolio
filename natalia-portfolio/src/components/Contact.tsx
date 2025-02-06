64573d20f9de5e9810875c766683079cce1951b7
furkanucgull <furkanucgull@gmail.com>
2025-02-06 22:23:20 +0300

    NavbarUpdate

--- natalia-portfolio/src/components/Contact.tsx
+++ natalia-portfolio/src/components/Contact.tsx
@@ -4,9 +4,7 @@ const Contact = () => {
     return (
         <main className=" lg:p-10 bg-[linear-gradient(135deg,#fff9c4,#52b69a_40%,rgba(100,159,117,0.385)_70%)] border-2 rounded-4xl mt-4 mx-4 lg:mt-10 lg:mx-10 ">
             <div >
-
                 <BusinessCard />
-
             </div>
         </main>
     );
